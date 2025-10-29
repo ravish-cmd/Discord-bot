1520a8ceabaf31238c43c85d25ebb50e74e2e3a1
ravish-cmd <ravishchoudhary2022@gmail.com>
2025-10-29 16:33:31 +0530

    index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.once("ready", () => {
+  console.log(`✅ Logged in as ${client.user.tag}`);
+});
+
+client.on("message", message => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("🏓 Pong!");
+  }
+});
+
+client.login("MTQyODMwMDU5MjgwNzQxNTgzOA.G9v7Lc.WnE6SY8phGzv55mfeBkeScVXacCHokow_YflRE");
